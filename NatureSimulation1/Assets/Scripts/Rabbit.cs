5d1034d64ed4eccc9ba9c4771c15b770a14c7ce8
NadunImalka <75896143+NadunImalka@users.noreply.github.com>
2021-07-29 18:40:52 +0530

    added Hunger Thirst
    +chooseTask

--- NatureSimulation1/Assets/Scripts/Rabbit.cs
+++ NatureSimulation1/Assets/Scripts/Rabbit.cs
@@ -6,8 +6,17 @@ using UnityEngine;
 
 public class Rabbit : MonoBehaviour
 {
+    private readonly String hungerTaskName = "food";
+    private readonly String thirstTaskName = "water";
+    private readonly String freeRoamName = "freeRoam";
+
+    private String task;
     private int foodLayer;
-    private Collider target;
+    private Transform target;
+    private bool atTheTarget;
+    private bool busy;
+
+    private float hunger, thirst;
     
     
     float sight = 5f;
@@ -16,51 +25,73 @@ public class Rabbit : MonoBehaviour
         foodLayer = LayerMask.NameToLayer("food");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vision();
-    }
+   
 
     void FixedUpdate()
     {
+
+        task = ChooseTask();
+        target = Vision(task);
         if(target != null) GetComponent<AIDestinationSetter>().target = target.transform;
+
+        hunger += 0.1f;
+        thirst += 0.1f;
     }
 
+    void EngageTask()
+    {
+        
+    }
+    String ChooseTask()
+    {
+        String chosenTask = freeRoamName;
+        if (hunger > thirst && hunger > 10)
+        {
+            chosenTask = hungerTaskName;
+        }
+        else if (thirst > hunger && thirst > 10)
+        {
+            chosenTask = thirstTaskName;
+        }
+
+        return chosenTask;
+    }
     void OnDrawGizmosSelected()
     {
-        Gizmos.color = Color.red;
+        Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position , sight);
     }
-
-    void Vision()
+    Transform Vision(String taskLayer)
     {
-        if (target != null) target.GetComponent<MeshRenderer>().material.color = Color.green;
-            
-            
+        Collider targetCollider = target.GetComponent<Collider>();
+        
+        
+        if (targetCollider != null) targetCollider.GetComponent<MeshRenderer>().material.color = Color.green;
+        
         float distance = Mathf.Infinity;
-        var colliders = Physics.OverlapSphere(transform.position, sight, 1 << foodLayer);
+        var colliders = Physics.OverlapSphere(transform.position, sight, 1 << LayerMask.NameToLayer(taskLayer));
         
         
         foreach (var collider in colliders)
         {
             if (Vector3.Distance(collider.transform.position , transform.position) < distance)
             {
-                target = collider;
+                targetCollider = collider;
                 distance = Vector3.Distance(collider.transform.position, transform.position);
             }
            
         }
-
-        if (target != null)
+        
+        if (targetCollider != null)
         {
-            target.GetComponent<MeshRenderer>().material.color = Color.red;
-            
+            targetCollider.GetComponent<MeshRenderer>().material.color = Color.red;
+            atTheTarget = Vector3.Distance(transform.position, targetCollider.transform.position) < 1.5f;
         }
         else
         {
             Debug.Log("theres no targets in the area");
         }
-        
+
+        return targetCollider.transform;
     }
 }
