4407f3efa69fe9db9ead0600fb2fb4832f35ab25
NadunImalka <75896143+NadunImalka@users.noreply.github.com>
2021-07-30 12:55:25 +0530

    added engage task
    + eat/drink  until  hunger/thirst is zero
    + eating/drinking variables

--- NatureSimulation1/Assets/Scripts/Rabbit.cs
+++ NatureSimulation1/Assets/Scripts/Rabbit.cs
@@ -10,46 +10,98 @@ public class Rabbit : MonoBehaviour
     private readonly String thirstTaskName = "water";
     private readonly String freeRoamName = "freeRoam";
 
+    private int eatDrinkThreshold = 30;
     private String task;
     private int foodLayer;
-    private Transform target;
+    private Collider target;
     private bool atTheTarget;
     private bool busy;
+    private bool eating, drinking;
+    private float distance;
 
-    private float hunger, thirst;
-    
-    
+    private float hunger, thirst = 30;
+
+
     float sight = 5f;
-    void Start()
-    {
-        foodLayer = LayerMask.NameToLayer("food");
-    }
 
-   
 
     void FixedUpdate()
     {
-
         task = ChooseTask();
         target = Vision(task);
-        if(target != null) GetComponent<AIDestinationSetter>().target = target.transform;
+        //set target in A* destination Setter
+        if (target != null) GetComponent<AIDestinationSetter>().target = target.transform;
+        
+        EngageTask();
 
-        hunger += 0.1f;
-        thirst += 0.1f;
+        DebugLog();
+        if (!busy)
+        {
+            hunger += 0.05f;
+            thirst += 0.05f;
+        }
     }
 
+    void DebugLog()
+    {
+        Debug.Log(hunger + " " + thirst + task + busy + atTheTarget + distance);
+    }
     void EngageTask()
     {
-        
+        if (atTheTarget)
+        {
+          
+            if (task == hungerTaskName && hunger > 1)
+            {
+                Debug.Log("HUNGERBUSY");
+                busy = true;
+                drinking = false;
+                eating = true;
+            }
+            else if (task == thirstTaskName && thirst > 1)
+            {
+                Debug.Log("THIRSTBUSY");
+                busy = true;
+                eating = false;
+                drinking = true;
+            }
+            else
+            {
+                eating = false;
+                drinking = false;
+                busy = false;
+            }
+            
+        }
+
+        if (task == freeRoamName)
+        {
+            drinking = false;
+            eating = false;
+            busy = false;
+        }
+
+        if (busy)
+        {
+            if (task == hungerTaskName && target.gameObject.layer == LayerMask.NameToLayer(task) && eating)
+            {
+                hunger -= 0.05f;
+            }
+            else if(task == thirstTaskName && target.gameObject.layer == LayerMask.NameToLayer(task) && drinking)
+            {
+                thirst -= 0.05f;
+            }
+        }
     }
     String ChooseTask()
     {
-        String chosenTask = freeRoamName;
-        if (hunger > thirst && hunger > 10)
+        String chosenTask = task;
+        if (!busy) chosenTask = freeRoamName;
+        if (hunger > thirst && hunger > eatDrinkThreshold)
         {
             chosenTask = hungerTaskName;
         }
-        else if (thirst > hunger && thirst > 10)
+        else if (thirst > hunger && thirst > eatDrinkThreshold)
         {
             chosenTask = thirstTaskName;
         }
@@ -59,16 +111,17 @@ public class Rabbit : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
-        Gizmos.DrawWireSphere(transform.position , sight);
+        Gizmos.DrawWireSphere(transform.position , 2.1f);
     }
-    Transform Vision(String taskLayer)
+    Collider Vision(String taskLayer)
     {
-        Collider targetCollider = target.GetComponent<Collider>();
+        Collider targetCollider = new Collider();
+        if(target != null) targetCollider = target.GetComponent<Collider>();
         
         
         if (targetCollider != null) targetCollider.GetComponent<MeshRenderer>().material.color = Color.green;
         
-        float distance = Mathf.Infinity;
+        distance = Mathf.Infinity;
         var colliders = Physics.OverlapSphere(transform.position, sight, 1 << LayerMask.NameToLayer(taskLayer));
         
         
@@ -85,13 +138,13 @@ public class Rabbit : MonoBehaviour
         if (targetCollider != null)
         {
             targetCollider.GetComponent<MeshRenderer>().material.color = Color.red;
-            atTheTarget = Vector3.Distance(transform.position, targetCollider.transform.position) < 1.5f;
+            atTheTarget = Vector3.Distance(transform.position, targetCollider.transform.position) < 2.1f;
         }
         else
         {
             Debug.Log("theres no targets in the area");
         }
 
-        return targetCollider.transform;
+        return targetCollider;
     }
 }
